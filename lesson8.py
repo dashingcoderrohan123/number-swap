efb97bcbff66a11c5e71cc7ec005d03f579214a4
dashingcoderrohan123 <anandrao23046@gmail.com>
2024-12-03 02:22:19 -0800

    Add files via upload

---
+++ lesson8.py
@@ -0,0 +1,20 @@
+# Initial values
+a = 1
+b = 2
+c = 3
+
+# Print initial values
+print("Before swap:")
+print("a =", a)
+print("b =", b)
+print("c =", c)
+
+# Interchanging values
+a, b, c = c, a, b
+
+# Print swapped values
+print("\nAfter swap:")
+print("a =", a)
+print("b =", b)
+print("c =", c)
+
